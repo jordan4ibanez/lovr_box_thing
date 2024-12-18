79edb55d6dbd482e4f75cb3eb7bd8a0603480dc4
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-12-18 01:25:39 -0500

    lazy mode

--- lovr-api.d.ts
+++ lovr-api.d.ts
@@ -1,7 +1,24 @@
-interface LOVRConfig {
-  
+interface LOVRConfigModules {
+  audio: boolean
+  data: boolean
+  event: boolean
+  graphics: boolean
+  headset: boolean
+  math: boolean
+  physics: boolean
+  system: boolean
+  thread: boolean
+  timer: boolean
 }
 
+interface LOVRConfig {
+  version: string
+  identity: string
+  saveprecedence: boolean
+  modules: LOVRConfigModules
+}
+
+/** @noSelf **/
 declare namespace lovr {
   function conf(t: LOVRConfig): void;
 }
--- src/conf.ts
+++ src/conf.ts
@@ -1,3 +1,5 @@
 lovr.conf = (t: LOVRConfig) => {
   print("hi");
-}
+  print(t.identity);
+  t.modules.audio = false;
+};
