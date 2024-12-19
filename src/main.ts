d1775f50c4bbddfc8999d45f7c5f914b10741f15
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-12-19 04:52:50 -0500

    and this

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,6 @@
 function main(): void {
   print("hi");
 
-  let te = lovr.math.newMat4();
-  te[1] = 5;
-  // todo: hmmm
-  // let blah = new Source();
+  let thing: Vec2 = lovr.math.vec2(0, 0);
 }
 
