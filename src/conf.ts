e9ae68ace3c9201b0088aa05f7a7201eb133db42
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-12-20 09:33:32 -0500

    I can't get the window icon working lol

--- src/conf.ts
+++ src/conf.ts
@@ -1,7 +1,14 @@
 lovr.conf = (t: LovrConfig) => {
+
+  print(lovr.filesystem.getWorkingDirectory());
+
+  t.version = "0.17.1";
+  t.identity = "default";
   t.modules.audio = false;
   t.audio.spatializer = "simple";
   t.window.resizable = true;
+  // let blah = lovr.filesystem.getWorkingDirectory() + "/textures/club_zero";
+  // t.window.icon = blah;
 
   conf = t.window;
 };
