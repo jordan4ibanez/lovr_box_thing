071cd7269c7a531960e60fbefb0011fab1745286
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-12-20 09:02:09 -0500

    there we go

--- lovr-api.d.ts
+++ lovr-api.d.ts
@@ -1791,7 +1791,7 @@ declare interface LovrConfig {
 }
 
 // Sneak this ambient variable in.
-declare let conf: LovrConfig | null;
+declare let conf: LovrConfigWindow;
 
 declare type LogLevel = "debug" | "info" | "warn" | "error";
 
