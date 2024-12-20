8c8e34f6a314baf7c79ac9bb1311a49813baa3a9
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-12-20 12:32:16 -0500

    a very annoying window lol

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,31 @@
 import * as window from "./lovr-window";
 
+let timer = 0;
+let smolify = true;
+
 lovr.load = () => {
 
-  jit.on();
-  jit.off();
+  // jit.on();
+  // jit.off();
 
-  print(window.focus());
-  print(window.getDisplayCount());
-  print(window.getDisplayDimensions(1)[0]);
-  print(window.getDisplayName(1));
-  print(window.getIcon());
-  print(window.getMode()[0]);
-  print(window.getOpacity());
-  print(window.getPosition()[0]);
-  print(window.getTitle());
-  window.setFullscreen(true, "desktop", 1);
-  window.setFullscreen(false, "desktop", 1);
-  window.setIcon("textures/");
+  // print(window.focus());
+  // print(window.getDisplayCount());
+  // print(window.getDisplayDimensions(1)[0]);
+  // print(window.getDisplayName(1));
+  // print(window.getIcon());
+  // print(window.getMode()[0]);
+  // print(window.getOpacity());
+  // print(window.getPosition()[0]);
+  // print(window.getTitle());
+  // window.setFullscreen(true, "desktop", 1);
+  // window.setFullscreen(false, "desktop", 1);
+  print("STARTING");
+  window.setIcon("textures/club_zero.png");
+  window.setMode(1024, 768);
+  // window.setOpacity(0.5)
+  // window.setPosition(100, 100);
+  window.setTitle("cool");
+
 
   // lovr.event.quit();
 };
@@ -25,9 +34,23 @@ lovr.load = () => {
 //   pass.text("hello!", 0, 1.7, -3, 0.5);
 // };
 
-// lovr.update = () => {
-//   lovr.event.quit();
-// };
+lovr.update = (delta: number) => {
+
+  timer = timer + delta;
+
+  print(timer);
+
+  if (timer > 1.0) {
+    if (smolify) {
+      window.minimize();
+    } else {
+      window.focus();
+      window.maximize();
+    }
+    smolify = !smolify;
+    timer = 0;
+  }
+};
 
 lovr.keypressed = (key: KeyCode) => {
   if (key == "escape") {
@@ -37,9 +60,8 @@ lovr.keypressed = (key: KeyCode) => {
 
 lovr.errhand = (message: string): (() => string | null) => {
   return () => {
-    print("error test:");
+    // print("error test:");
     print(message);
-
-    return "hi";
+    return "";
   };
 };
