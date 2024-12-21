4af608a61776602920606aee3257c544ef1e4bde
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-12-21 09:30:57 -0500

    All this thing

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,22 @@ import * as mouse from "./lovr-mouse";
 import * as keyboard from "./lovr-keyboard";
 
 lovr.load = () => {
+  mouse.getPosition();
 
 };
 
 lovr.update = (delta: number) => {
-
+  // print(keyboard.isDown("a", "s"));
+  if (keyboard.isDown("a", "b")) {
+    print("hi");
+  }
 };
 
 lovr.keypressed = (key: KeyCode) => {
   if (key == "escape") {
     lovr.event.quit();
   }
+  // print(keyboard.isDown("a", "b"));
 };
 
 lovr.errhand = (message: string): (() => string | null) => {
