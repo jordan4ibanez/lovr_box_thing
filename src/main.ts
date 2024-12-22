1cfee061e26dcd0719336b4577dbcf1cce7b687f
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-12-22 07:00:19 -0500

    Make a ground box

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import * as window from "./lovr-window";
 import * as keyboard from "./keyboard";
 
 let world: World;
+let boxes: Array<Collider> = [];
 
 keyboard.setKeyPressedCallback("escape", () => {
   lovr.event.quit();
@@ -9,6 +10,9 @@ keyboard.setKeyPressedCallback("escape", () => {
 
 lovr.load = () => {
   world = lovr.physics.newWorld();
+  let ground = world.newBoxCollider(0, 1, 0, 1, 1, 1);
+  ground.setKinematic(true);
+  boxes.push(ground);
 };
 
 lovr.update = (delta: number) => {
