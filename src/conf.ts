987db2806051be11f5763186704e880cc4a26ec0
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-12-22 07:53:24 -0500

    big problem

--- src/conf.ts
+++ src/conf.ts
@@ -11,6 +11,8 @@ lovr.conf = (t: LovrConfig) => {
   t.window.icon = "/textures/club_zero_icon/club_zero.png";
   t.window.width = 2000;
   t.window.height = 1000;
+  t.graphics.vsync = false;
+
 
   conf = t.window;
 };
